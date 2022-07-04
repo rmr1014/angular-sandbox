fe34019fb82fdcb628222d5ade0fb5cdc5f4463a
Ryan Ross <rmr1014@gmail.com>
2022-07-04 11:14:27 -0400

    Initial wiring of routes

--- sandbox/src/app/app-routing.module.ts
+++ sandbox/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularWelcomeComponent } from './angular-welcome/angular-welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  {path: 'welcome', component: AngularWelcomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
