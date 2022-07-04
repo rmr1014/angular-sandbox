4ca4b1179d7bde62a04cd122a4edad08dc81842d
Ryan Ross <rmr1014@gmail.com>
2022-07-04 11:28:43 -0400

    Add routing for ButtonComponent

--- sandbox/src/app/app-routing.module.ts
+++ sandbox/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularWelcomeComponent } from './angular-welcome/angular-welcome.component';
+import { ButtonComponent } from './button/button.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
-  {path: 'welcome', component: AngularWelcomeComponent}
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'button', component: ButtonComponent },
+  { path: 'welcome', component: AngularWelcomeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
