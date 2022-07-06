6ab4a89879cba28eb0275d958c74d933856aa81e
Ryan Ross <rmr1014@gmail.com>
2022-07-05 21:27:49 -0400

    Adding BadgeComponent

--- sandbox/src/app/app-routing.module.ts
+++ sandbox/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AngularWelcomeComponent } from './angular-welcome/angular-welcome.component';
+import { BadgeComponent } from './badge/badge.component';
 import { ButtonComponent } from './button/button.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'badge', component: BadgeComponent },
   { path: 'button', component: ButtonComponent },
   { path: 'welcome', component: AngularWelcomeComponent },
 ];
--- sandbox/src/app/app.module.ts
+++ sandbox/src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularWelcomeComponent } from './angular-welcome/angular-welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatIconModule } from '@angular/material/icon';
 import { ButtonComponent } from './button/button.component';
-import {MatDividerModule} from '@angular/material/divider';
+import { BadgeComponent } from './badge/badge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AngularWelcomeComponent,
-    ButtonComponent
+    ButtonComponent,
+    BadgeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatBadgeModule,
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
